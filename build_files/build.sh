a31575eefe89f2c32bf81ca16569c43b16da106d
Hippie Hacker <hh@ii.coop>
2025-09-25 11:34:41 -0400

    chore: add touchegg

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux touchegg xdotools
 
 # Use a COPR Example:
 #
@@ -20,5 +20,7 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
+#
+rpm-ostree override install touchegg xdotool vim
 
 systemctl enable podman.socket
